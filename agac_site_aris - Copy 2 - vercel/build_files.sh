686e975fd1ee7825845ec2d41e86a4838a08ba67
Kubrashn <123374616+Kubrashn@users.noreply.github.com>
2024-07-22 14:16:51 +0300

    n

--- agac_site_aris - Copy 2 - vercel/build_files.sh
+++ agac_site_aris - Copy 2 - vercel/build_files.sh
@@ -2,8 +2,6 @@
 set -e
 
 echo "BUILD START"
-python3.9 -m ensurepip --upgrade  # Pip'in kurulu olduğunu kontrol edin ve güncelleyin
-python3.9 -m pip install --upgrade pip  # Pip'i güncelleyin
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
